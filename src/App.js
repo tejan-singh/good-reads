3f4e134e7e6ae633c63b45976e999a3896d6045e
Tejan Singh <tstejan@gmail.com>
2022-09-25 11:26:27 +0530

    feat: added filter functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [bookList, setBookList] = useState([])
+
+  const books = [
+    {
+      id: 1,
+      title: 'Eloquent JavaScript',
+      rating: 4 / 5,
+      category: "javascript" 
+    },
+
+    {
+      id: 2,
+      title: "You Don't Know JS",
+      rating: 3.5 / 5,
+      category: "javascript" 
+    },
+
+    {
+      id: 3,
+      title: 'Shiva Trilogy',
+      rating: 5/5,
+      category: "fiction" 
+    },
+
+    {
+      id: 4,
+      title: "Harry Potter and the Sorcerer's Stone",
+      rating: 4.5/5,
+      category: "fiction" 
+    },
+
+    {
+      id: 5,
+      title: 'Never Split the Difference',
+      rating: 3.5/5,
+      category: "business" 
+    },
+
+    {
+      id: 6,
+      title: 'Loonshots',
+      rating: 5/5,
+      category: "business" 
+    },
+  ]
+
+  // to pull out unique categories and avoid repetation
+  const categories = [ ...new Set(books.map((book)=>{return book.category})) ] // to convert to an array
+
+  const handleClick = (category) => {
+    const result = books.filter((book)=>{return book.category === category})
+    console.log(result)
+    setBookList(result)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        <span>📚</span> goodBooks
+      </h1>
+      <p>Checkout my favorite books. Select a genre to get started</p>
+
+      {categories.map((category)=>{
+        return <button key ={category} className="btn" onClick={()=>{handleClick(category)}}>{category}</button>
+      })}
+      <hr />
+
+
+
+      {bookList.map((book, index)=>{
+        return <div className="container" key={index}>
+        <p>{book.title}</p>
+        <small>{book.rating}</small>
+        </div>
+      })}
+
     </div>
   );
 }
